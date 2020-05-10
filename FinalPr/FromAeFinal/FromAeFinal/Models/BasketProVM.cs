ec5e92105c963911f5a442d23eefbb0484de456a
Rafigjg <rafigjg@code.edu.az>
2020-05-10 13:34:16 +0400

    add

---
+++ FinalPr/FromAeFinal/FromAeFinal/Models/BasketProVM.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FromAeFinal.Models
+{
+    public class BasketProVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string MainImage { get; set; }
+        public int Count { get; set; }
+    }
+}
